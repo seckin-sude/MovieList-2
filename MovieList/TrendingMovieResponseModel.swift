e02eff2f11354d2e97e0ea030a94661a442bceeb
seckin-sude <seckin.sude@metu.edu.tr>
2024-08-27 15:37:42 +0300

    secondCommit

---
+++ MovieList/TrendingMovieResponseModel.swift
@@ -0,0 +1,23 @@
+//
+//  TrendingMovieResponseModel.swift
+//  MovieList
+//
+//  Created by Apple Egitim on 27.08.2024.
+//
+
+import Foundation
+
+struct TrendingMoviesResponseModel :Codable {
+    let page:Int
+    let results: [MovieResponse]
+    
+}
+struct MovieResponse: Codable {
+    let id: Int
+    let title: String
+    let overview: String
+    let poster_path: String
+    let release_date: String
+    let vote_average: Double
+    
+}
--- MovieList/ViewController.swift
+++ MovieList/ViewController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var myCollectionView:UICollectionView!
+    
+    let ACCESS_TOKEN = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzhkNmE5MzY0ZWNlYzBhOTA0ZTI2NWY0MmY3OThkNyIsIm5iZiI6MTcyNDc0NDM0MC4wMDAzMTksInN1YiI6IjY2Y2Q4MDY5ZmI5ZDU5ZmJkNDZhYTE5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SWSFm2HFd6wohI9fhgSfAenW6aHaheGEEIB7FDyPOuo"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +24,69 @@ class ViewController: UIViewController {
             flowLayout.scrollDirection = .horizontal
             
         }
+        
+        guard let url = URL(string: "https://api.themoviedb.org/3/trending/movie/day")
+        else {
+            printContent("invalid url")
+            return
+        }
+        var request = URLRequest(url:url)
+        
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue(ACCESS_TOKEN, forHTTPHeaderField: "Authorization")
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Error while fetching : \(error)")
+                return
+            }
+            guard let data = data, let httpResponse = response as?
+                    HTTPURLResponse, httpResponse.statusCode == 200 else  {
+                print ("status code is NOT successfull()")
+                return
+            }
+            
+            do  {
+                let decoder = JSONDecoder()
+                let myData = try decoder.decode(TrendingMoviesResponseModel.self, from:data)
+                let firstMovie = myData.results[0]
+                print("data response movie title:  \(firstMovie.title)")
+                
+            } catch {
+                print("ERROR DECODING JSON: \(error)")
+                
+            }
+        }
+        task.resume()
+       
     }
-
+    
+    
+    func getMoviesApi() async  {
+        
+        let url = URL(string: "https://api.themoviedb.org/3/trending/movie/day")!
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
+        let queryItems: [URLQueryItem] = [
+            URLQueryItem(name: "language", value: "en-US"),
+        ]
+        components.queryItems = components.queryItems.map { $0 + queryItems } ?? queryItems
+        
+        var request = URLRequest(url: components.url!)
+        request.httpMethod = "GET"
+        request.timeoutInterval = 10
+        request.allHTTPHeaderFields = [
+            "accept": "application/json",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzhkNmE5MzY0ZWNlYzBhOTA0ZTI2NWY0MmY3OThkNyIsIm5iZiI6MTcyNDc0NDM0MC4wMDAzMTksInN1YiI6IjY2Y2Q4MDY5ZmI5ZDU5ZmJkNDZhYTE5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SWSFm2HFd6wohI9fhgSfAenW6aHaheGEEIB7FDyPOuo"
+        ]
+        do {
+            
+            let (data, _) = try await URLSession.shared.data(for: request)
+            print(String(decoding: data, as: UTF8.self))
+        } catch  {
+            print ("GOT ERROR")
+        }
+    }
 }
+
 
 extension ViewController:UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
